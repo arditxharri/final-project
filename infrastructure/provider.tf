e42a51ecf321d1157df290789a752d72fa1133ce
Ardit Xharri <108690361+arditxharri@users.noreply.github.com>
2024-01-21 09:55:17 +0100

    Update provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,21 +1,21 @@
 provider "aws" {
   region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 }
 
 terraform {
-  cloud {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
     organization = "axharri"
 
     workspaces {
       name = "axh"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
     }
   }
 }
